eabecb26fe20d17eb7b388ec5aa8f28c22285b35
dennishooo <hkhdennis@gmail.com>
2023-05-08 22:12:07 +0800

    Add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [TodosModule],
+  imports: [PrismaModule.forRoot(), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
