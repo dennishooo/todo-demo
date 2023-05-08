c22a853340d883001804a28309e71e6eee3ef5d8
dennishooo <hkhdennis@gmail.com>
2023-05-09 04:03:49 +0800

    Add prisma exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { PrismaService } from 'nestjs-prisma';
-import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter, PrismaService } from 'nestjs-prisma';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter(httpAdapter, {
+      P1012: HttpStatus.BAD_REQUEST,
+      P2000: HttpStatus.BAD_REQUEST,
+      P2001: HttpStatus.NOT_FOUND,
+      P2002: HttpStatus.CONFLICT,
+      P2003: HttpStatus.CONFLICT,
+      P2025: HttpStatus.NOT_FOUND,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('To Do App')
     .setDescription('API description')
